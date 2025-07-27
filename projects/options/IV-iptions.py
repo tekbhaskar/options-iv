39a225d74da974273428f96402bead38a1592cd8
unknown <tekbhaskar@gmail.com>
2025-07-27 17:52:17 -0500

    modified option.py

--- projects/options/IV-iptions.py
+++ projects/options/IV-iptions.py
@@ -1,8 +1,8 @@
 
 import streamlit as st
 import pandas as pd
-from sp500_options import get_sp500_tickers, get_sp500_stock_data
-from nyse_options import get_nyse_tickers, get_nyse_stock_data
+from sp500_options import get_sp500_tickers, get_sp500_stock_data as sp500
+from nyse_options import get_nyse_tickers, get_nyse_stock_data as nyse
 
 
 st.title("🔥 Implied Volatility Screener for S&P and NYSE")
@@ -11,23 +11,23 @@ tab1, tab2,tab3,tab4 = st.tabs(["🔥 S&P 500 High IV Stocks", "❄️ S&P 500 L
 
 with tab1:
     st.subheader("S&P 500 Highest Implied Volatility")
-    high_iv = df_sp500.sort_values(by="Avg IV (%)", ascending=False).head(20)
+    high_iv = sp500.df_sp500.sort_values(by="Avg IV (%)", ascending=False).head(20)
     st.dataframe(high_iv.reset_index(drop=True), use_container_width=True)
 
 with tab2:
     st.subheader("S&P 500 Lowest Implied Volatility")
-    low_iv = df_sp500.sort_values(by="Avg IV (%)", ascending=True).head(20)
+    low_iv = sp500.df_sp500.sort_values(by="Avg IV (%)", ascending=True).head(20)
     st.dataframe(low_iv.reset_index(drop=True), use_container_width=True)
 
 
 with tab3:
     st.subheader("Top 20 NYSE Stocks by Highest Implied Volatility")
-    high_iv = df_nyse.sort_values(by="Avg IV (%)", ascending=False).head(20)
+    high_iv = nyse.df_nyse.sort_values(by="Avg IV (%)", ascending=False).head(20)
     st.dataframe(high_iv.reset_index(drop=True), use_container_width=True)
 
 with tab4:
     st.subheader("Top 20 NYSE Stocks by Lowest Implied Volatility")
-    low_iv = df_nyse.sort_values(by="Avg IV (%)", ascending=True).head(20)
+    low_iv = nyse.df_nyse.sort_values(by="Avg IV (%)", ascending=True).head(20)
     st.dataframe(low_iv.reset_index(drop=True), use_container_width=True)
     
 
